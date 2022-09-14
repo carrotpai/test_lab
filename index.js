441f25d5f79c208344aa3e6667cf4e6fa4dece20
carrotpai <55906326+carrotpai@users.noreply.github.com>
2022-09-14 21:07:56 +0800

    lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("strange text")
+const date1 = new Date('January 1, 2023 00:00:00');
+console.log(date1)
+console.log(new Date())
+const date2 = new Date(Math.abs(new Date().getTime() - date1.getTime()))
+console.log(date2)
+console.log(`months: ${date2.getMonth()}, days: ${date2.getDate()}, hours: ${date2.getUTCHours()}, minutes: ${date2.getMinutes()} to new year`)
